d1b2ed67058648481b58ea905319ec1c596788f1
= <65097349+AndersonSiquelliTeofilo@users.noreply.github.com>
2024-02-06 22:50:55 -0300

    fix: reestructure the folder

---
+++ scripts/main.js
@@ -0,0 +1,7 @@
+import AppMenu from './app/components/AppMenu.js';
+import AppFooter from './app/components/AppFooter.js';
+import Task from './app/components/Task.js';
+
+customElements.define("app-menu", AppMenu);
+customElements.define("app-footer", AppFooter);
+customElements.define("task-post", Task);
